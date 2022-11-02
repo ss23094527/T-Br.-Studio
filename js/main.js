71b00f56a67920ccb3230b829503980d255c15e2
ss23094527 <104896444+ss23094527@users.noreply.github.com>
2022-11-02 23:31:03 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,71 @@
+
+
+
+ // go to top
+jQuery(document).ready(function($) {
+	
+	$('.top').click(function(event) {
+		event.preventDefault();
+
+		$('html, body').animate({
+			scrollTop: 0
+		},
+			1000);
+	});
+
+});
+
+
+
+ // loading animation
+$(window).on('load', function() { // makes sure the whole site is loaded 
+	$('#status').fadeOut(); // will first fade out the loading animation 
+	$('#preloader').delay(250).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+	$('body').delay(250).css({'overflow':'visible'});
+  })
+
+  $(document).ready(function() {
+	$('#fullpage').fullpage();
+});
+
+//文字飛入
+$(document).scroll(function () {
+    let y = $(this).scrollTop();
+    if (y > 150 ) {
+        $('.painters').fadeIn();
+      
+    } else {
+		$('.painters').fadeOut();
+        
+    }
+
+	if (y > 400 ) {
+        $('.painter-icon-A').fadeIn();
+		$('.painter-icon-B').fadeIn();
+      
+    } else {
+		$('.painter-icon-A').fadeOut();
+		$('.painter-icon-B').fadeOut();
+        
+    }
+
+	if (y > 600 ) {
+        $('.about_title').fadeIn();
+		$('.about_title2').fadeIn();
+      
+    } else {
+		$('.about_title').fadeOut();
+		$('.about_title2').fadeOut();
+        
+    }
+
+
+
+    if (y > 780) {
+        $('.portfoliotext').fadeIn();
+    } else {
+        $('.portfoliotext').fadeOut();
+    }
+
+    
+});
